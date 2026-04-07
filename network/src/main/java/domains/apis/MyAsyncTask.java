f468fde720e75d1fb530a05eeceeae1702aa892a
anekk <aneckalarionova@gmail.com>
2026-04-07 15:42:26 +0500

    Разработанный класс выполнения запросов. Казаков

---
+++ network/src/main/java/domains/apis/MyAsyncTask.java
@@ -0,0 +1,30 @@
+package domains.apis;
+
+import androidx.loader.content.AsyncTaskLoader;
+
+import domains.callbacks.MyResponseCallback;
+import android.os.AsyncTask;
+
+public class MyAsyncTask extends AsyncTask<Void, Void, String> {
+    protected MyResponseCallback callback;
+
+    public  MyAsyncTask(MyResponseCallback callback){
+        this.callback = callback;
+    }
+
+    @Override
+    protected String doInBackground(Void... voids){
+        return "";
+    }
+
+    @Override
+    protected void onPostExecute(String result){
+        super.onPostExecute(result);
+        if (callback != null) {
+            if (result != null && !result.startsWith("Error"))
+                callback.onCompile(result);
+            else
+                callback.onError(result);
+        }
+    }
+}
